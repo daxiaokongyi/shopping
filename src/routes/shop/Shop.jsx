70b66c6b3313e5493447911f58a0490dc14d1b0a
Hang Jin <hangjin@hangs-mbp.lan>
2024-09-17 00:29:29 -0700

    finished redux thunk-173

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -5,18 +5,22 @@ import Category from '../category/Category';
 import { useEffect } from 'react';
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase';
 import { useDispatch } from 'react-redux';
-import { setCategories } from '../../store/categories/categoryAction';
+// import { setCategories } from '../../store/categories/categoryAction';
+import { fetchCategoriesAsync } from '../../store/categories/categoryAction';
+
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments('categories');
-      dispatch(setCategories(categoriesArray));
+    // const getCategoriesMap = async () => {
+      // const categoriesArray = await getCategoriesAndDocuments('categories');
+      // dispatch(setCategories(categoriesArray));
+      // dispatch(fetchCategoriesAsync);
+      dispatch(fetchCategoriesAsync());
     }
-    getCategoriesMap();
-  }, []);
+    // getCategoriesMap();
+  , []);
 
   return (
     <Routes>
